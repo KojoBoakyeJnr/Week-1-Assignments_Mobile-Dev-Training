6dab14da7d4c0b9ce3692914575684af41c436eb
KojoBoakye <kojoboakyejnr@outlook.com>
2025-07-28 00:55:51 +0000

    fixing errors

--- Sam_Boakye_Assignment2.dart
+++ Sam_Boakye_Assignment2.dart
@@ -1,7 +1,7 @@
 void main() {
   BigInt square(final int n) {
     if (n > 64 || n < 1) {
-      print('Invalid square number, must be between 1 and 56');
+      print('Invalid square number, must be between 1 and 64');
       return BigInt.from(0);
     } else {
       return BigInt.from(2).pow(n - 1);
@@ -9,14 +9,13 @@ void main() {
   }
 
   BigInt total() {
-    BigInt result = BigInt.from(0);
-    for (int square = 0; square < 65; square++) {
-      result += BigInt.from(2).pow(square - 1);
+    BigInt sum = BigInt.from(0);
+    for (int i = 1; i <= 64; i++) {
+      sum += BigInt.from(2).pow(i - 1);
     }
-
-    return result;
+    return sum;
   }
 
-  print(square(5));
+  print(square(1));
   print(total());
 }
